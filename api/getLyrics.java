5fdb587f9fbda98cc57f856d43a5a2f5840770d0
amelia1248 <ameliawu.wu@mail.utoronto.ca>
2023-10-01 19:50:52 -0400

    final java code example

--- api/getLyrics.java
+++ api/getLyrics.java
@@ -1,19 +1,21 @@
 package api;
-
-import entity.Grade;
-import entity.Team;
 import okhttp3.*;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.Arrays;
+
 public class getLyrics {
     private static final String API_URL = "https://api.lyrics.ovh/v1/artist/title";
-    @Override
-    public static void main(String artist, String title) {
-        ...
 
+    public void main(String artist, String title) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.lyrics.ovh/v1/artist/title?artist=Coldplay&title=Adventure%20of%20a%20Lifetime")
+                .get()
+                .build();
+
+        Response response = client.newCall(request).execute();
+        System.out.println(response);
     }
 }
+
