28721cae7b88b7aa7986a747c673d7c62cc5adb8
Celine C <celine.chiu@mail.utoronto.ca>
2023-11-22 10:17:36 -0500

    Implemented Player entity

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,2 +1,19 @@
-package api.src.entities;public class Player {
+package api.src.entities;
+public class Player {
+
+    private String name;
+    private int score;
+
+    public String getName() {return name;}
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getScore() {return score;}
+
+    // value for point to be determined by taking the difference of the popularity of the two songs and assigning point value accordingly. Point values to be decided later.
+    public void setScore(int point) {
+        this.score = score + point;
+    }
+
 }
