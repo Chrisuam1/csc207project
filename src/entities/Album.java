208507058845c313324ab7f958b9264464e8c4cc
Celine C <celine.chiu@mail.utoronto.ca>
2023-11-22 10:16:47 -0500

    removed incorrect implementation of Album

--- src/entities/Album.java
+++ src/entities/Album.java
@@ -1,30 +1,5 @@
 package api.src.entities;
 public class Album {
-
-    private final String title;
-    private final Song[] songs;
-
-    private final String artist;
-
-    private final String cover;
-
-    private final Integer yearReleased;
-
-    Album(String title, Song[] songs, String artist, String cover, Integer yearReleased) {
-        this.title = title;
-        this.songs = songs;
-        this.artist = artist;
-        this.cover = cover;
-        this.yearReleased = yearReleased;
+    Album() {
     }
-
-    public String getTitle() {return title; }
-
-    public Song[] getSongs() {return songs; }
-
-    public String getArtist() {return artist; }
-
-    public String getCover() {return cover; }
-
-    public Integer getYearReleased() {return yearReleased; }
 }
