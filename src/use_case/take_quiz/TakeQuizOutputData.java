c1077d9dd1e8479d910ef3eb9a6b507273b8b764
Francisco Caber <caber.fr@gmail.com>
2023-11-25 12:56:26 -0500

    added TakeQuizOutputData

---
+++ src/use_case/take_quiz/TakeQuizOutputData.java
@@ -0,0 +1,19 @@
+package api.src.use_case.take_quiz;
+
+public class TakeQuizOutputData {
+    final private int score;
+    final private int numQuestions;
+
+    public TakeQuizOutputData(int score, int numQuestions) {
+        this.score = score;
+        this.numQuestions = numQuestions;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getNumQuestions() {
+        return numQuestions;
+    }
+}
