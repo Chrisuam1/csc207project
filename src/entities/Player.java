0d709195c1ead723ece83017b57789c7cf80d61f
Francisco Caber <caber.fr@gmail.com>
2023-11-27 00:27:48 -0500

    made Player implement comparable, based on Player.score, overhauled Leaderboard class to use ArrayLists instead of primitive arrays.

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -1,16 +1,29 @@
 package entities;
-public class Player {
 
+public class Player implements Comparable<Player>{
+
+    private static int ID_GENERATOR = 0;
+    private final int id;
     private final String name;
     private double score;
 
     public Player(String name) {
+        this.id = ID_GENERATOR++;
         this.name = name;
         score = 0.0;
     }
 
-    public String getName() {return name;}
-    public double getScore() {return score;}
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getScore() {
+        return score;
+    }
 
     // value for point to be determined by taking the difference of the popularity of the two songs
     // and assigning point value accordingly. Point values to be decided later.
@@ -21,4 +34,11 @@ public class Player {
     public void addScore(double score) {
         this.score += score;
     }
+
+    // Allows players to be sorted based on score, used in Leaderboard
+    @Override
+    public int compareTo(Player otherPlayer) {
+        return Double.compare(this.score, otherPlayer.score);
+    }
+
 }
