de4f04246780b62f40852c944c14e47fd1886faf
Francisco Caber <caber.fr@gmail.com>
2023-11-27 01:19:52 -0500

    added constructor to Quiz class, added methods addQuestion and addQuestionList

--- src/entities/Quiz.java
+++ src/entities/Quiz.java
@@ -2,31 +2,29 @@ package entities;
 import java.util.ArrayList;
 
 public class Quiz {
-    private int quizType;
     private ArrayList<Question> questions;
     private int numQuestions;
 
-    public int getQuizType() {return quizType;}
-
-    public void setQuizType(int quizType){
-        this.quizType = quizType;
+    public Quiz() {
+        questions = new ArrayList<>();
+        numQuestions = 0;
     }
 
-    public ArrayList<Question> getQuestions() {return questions;}
-
-    public int getNumQuestions(){return numQuestions;}
-
-    public void setNumQuestions(int NumQuestions) {
-        this.numQuestions = numQuestions;
+    public void addQuestion(Question question) {
+        questions.add(question);
+        numQuestions += 1;
     }
 
-    // setter for questions
-    public void generateQuestions(Question question) {
-        int i = 0;
-        while (i <= numQuestions) {
-            // TODO: implement setQuestion
-            //question.setQuestion();
-            i++;
-        }
+    public void addQuestionList(ArrayList<Question> questionList) {
+        questions.addAll(questionList);
+        numQuestions += questionList.size();
+    }
+
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+
+    public int getNumQuestions() {
+        return numQuestions;
     }
 }
