e7a3527ebf74e6fce9bd7b819191b54dd9487e07
Chrisuam1 <cloroxbleachperson@gmail.com>
2023-12-04 07:36:22 -0500

    moved a button around, started main menu implementation

--- src/view/MainMenuView.java
+++ src/view/MainMenuView.java
@@ -18,10 +18,14 @@ public class MainMenuView extends JPanel implements ActionListener {
     JLabel backgroundLabel = new JLabel(backgroundImage);
     JLabel exitButton = new JLabel("exit");
 
+    Dimension ss = Toolkit.getDefaultToolkit ().getScreenSize ();
+
+
     private final int MENU_ITEM_WIDTH = 300;
     private final int MENU_ITEM_HEIGHT = 400;
 
-    MenuItemJLabel startQuizButton = new MenuItemJLabel("start quiz", startQuizIcon, MENU_ITEM_WIDTH, MENU_ITEM_HEIGHT);
+    MenuItemJLabel startQuizHover = new MenuItemJLabel("start quiz", startQuizIcon, MENU_ITEM_WIDTH, MENU_ITEM_HEIGHT);
+    JButton startQuizButton = new JButton();
 
     // TODO: make constructor with controller and viewModel argument
     public MainMenuView() {
@@ -35,32 +39,20 @@ public class MainMenuView extends JPanel implements ActionListener {
         label1.setBackground(Color.red);
         label1.setOpaque(true);
         label1.setPreferredSize(new Dimension(100,100));
+        label1.setHorizontalAlignment(SwingConstants.CENTER);
+        label1.setVerticalAlignment(SwingConstants.CENTER);
 
-        backgroundLabel.add(startQuizButton);
+        backgroundLabel.add(startQuizHover);
         //startQuizButton.setBackground(Color.pink);
         //startQuizButton.setOpaque(true);
 
-        startQuizButton.setBounds(0,0,MENU_ITEM_WIDTH,MENU_ITEM_HEIGHT);
+        startQuizHover.setBounds(ss.width/2 - MENU_ITEM_WIDTH / 2, ss.height/2 - MENU_ITEM_HEIGHT / 2,MENU_ITEM_WIDTH,MENU_ITEM_HEIGHT);
+        startQuizButton.setBounds(ss.width/2 - MENU_ITEM_WIDTH / 2, ss.height/2 - MENU_ITEM_HEIGHT / 2,MENU_ITEM_WIDTH,MENU_ITEM_HEIGHT);
+        startQuizButton.addActionListener(this);
 
-
-
-
-//        this.setBackground(themeColor);
-//        this.setLayout(new BorderLayout());
-//
-//        JLabel title = new JLabel("presented by");
-//        this.add(title);
-//        title.setHorizontalAlignment(SwingConstants.CENTER);
-//        title.setVerticalAlignment(SwingConstants.CENTER);
-//        title.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 50));
-//        title.setHorizontalTextPosition(JLabel.CENTER);
-//        title.setVerticalTextPosition(JLabel.TOP);
-//        title.setForeground(Color.black);
-//        title.setIcon(icon);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
     }
 }
---
+++ src/view/QuestionView.java
@@ -0,0 +1,17 @@
+package view;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class QuestionView extends JPanel implements ActionListener {
+    public final String viewName = "main menu";
+    public QuestionView() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
